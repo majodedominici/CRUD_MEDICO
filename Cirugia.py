2827e153e69e445e04efccdae89cb5cfdff85249
María José Dedominici <mjdedominici@gmail.com>
2021-10-16 15:00:57 -0300

    Arme el metodo para delegar la responsabilidad de mostrarse por pantalla a cada objeto

--- Cirugia.py
+++ Cirugia.py
@@ -3,7 +3,18 @@ from Reserva import Reserva
 
 class Cirugia(Reserva):
     def __init__(self, fecha, hora, nombrePaciente, diagnostico, numeroQuirofano, horasAyuno, instrumentadorQuirurgico):
-        Reserva.__init__(self, fecha, hora, nombrePaciente, diagnostico, numeroQuirofano, horasAyuno, instrumentadorQuirurgico)
+        Reserva.__init__(self, fecha, hora, nombrePaciente, diagnostico)
         self.numeroQuirofano = numeroQuirofano
         self.horasAyuno = horasAyuno
         self.instrumentadorQuirurgico = instrumentadorQuirurgico
+
+    def mostrarPorPantalla(self):
+        print(f"Id: {self.id}"
+              f"\nFecha: {self.fecha}"
+              f"\nHora: {self.hora}"
+              f"\nDiagnostico: {self.diagnostico}"
+              f"\nPaciente: {self.nombrePaciente}"
+              f"\nNumero de Quirofano: {self.numeroQuirofano}"
+              f"\nHora de Ayuno: {self.horasAyuno}"
+              f"\nInstrumentador Quirurgico: {self.instrumentadorQuirurgico}"
+              f"\n--")
--- Consulta.py
+++ Consulta.py
@@ -4,3 +4,12 @@ class Consulta(Reserva):
     def __init__(self,fecha,hora,nombrePaciente,diagnostico,numeroConsultorio):
         Reserva.__init__(self,fecha,hora,nombrePaciente,diagnostico)
         self.numeroConsultorio = numeroConsultorio
+
+    def mostrarPorPantalla(self):
+        print(f"Id: {self.id}"
+              f"\nFecha: {self.fecha}"
+              f"\nHora: {self.hora}"
+              f"\nConsultorio: {self.numeroConsultorio}"
+              f"\nDiagnostico: {self.diagnostico}"
+              f"\nPaciente: {self.nombrePaciente}"
+              f"\n--")
