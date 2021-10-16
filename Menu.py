e31b75b0481acd32a4d598a158b773b764c892cf
María José Dedominici <mjdedominici@gmail.com>
2021-10-16 15:02:08 -0300

    Modifiqué el menu,lo dejé más legible y extraje funcionalidades

--- Menu.py
+++ Menu.py
@@ -3,6 +3,9 @@ import sys
 from RepositorioConsultas import RepositorioConsultas
 from Consulta import Consulta
 
+from RepositorioCirugia import RepositorioCirugia
+from Cirugia import Cirugia
+
 class Menu:
     '''muestra el menu y lanza la función elegida'''
 
@@ -19,28 +22,25 @@ class Menu:
             "9": self.salir
         }
         self.repositorioConsultas = RepositorioConsultas()
+        self.repositorioCirugias = RepositorioCirugia()
 
     def mostrar_menu(self):
         '''Muestra el menú de opciones'''
         print("""
 Consultas y Cirugías:
-
 -- Ingresos --
 1. Ingresar consulta
 2. Ingresar cirugía
-
 -- Modificaciones --
 3. Modificar consulta
 4. Modificar cirugía
-
 -- Eliminar --
 5. Eliminar consulta
 6. Eliminar cirugía
-
 -- Listar --
 7. Listar consulta
 8. Listar cirugía
-
+--
 9. Salir
 """)
 
@@ -52,6 +52,7 @@ Consultas y Cirugías:
             accion = self.opciones.get(opcion)
             if accion:
                 accion()
+                input("Presione enter para continuar.")
             else:
                 print("{0} no es una opción válida".format(opcion))
 
@@ -60,47 +61,69 @@ Consultas y Cirugías:
         sys.exit(0)
 
     def agregar_consulta(self):
-        hora= input("Ingrese la hora: ")
-        fecha = input("Ingrese la fecha: ")
-        nombrePaciente = input("Ingrese el nombre del paciente: ")
-        diagnostico = input("Ingrese el diagnostico: ")
-        numeroConsultorio = input("Ingrese el numero de consultorio donde será atendido: ")
-        nuevaConsulta = Consulta(fecha,hora,nombrePaciente,diagnostico,numeroConsultorio)
-        self.repositorioConsultas.store(nuevaConsulta)
+        nuevaConsulta = self.nuevaReserva("consulta")
+        idConsultaAgregada = self.repositorioConsultas.store(nuevaConsulta)
+        if idConsultaAgregada == 0:
+            print ("Error al guardar la consulta nueva")
+        else:
+            print (f"Consulta nº{idConsultaAgregada} guardada con éxito")
 
     def agregar_cirugia(self):
-        print("TBD")
+        nuevaCirugia = self.nuevaReserva("cirugia")
+        idCirugiaAgregada = self.repositorioCirugias.store(nuevaCirugia)
+        if idCirugiaAgregada == 0:
+            print ("Error al guardar la cirugia nueva")
+        else:
+            print (f"Cirugia nº{idCirugiaAgregada} guardada con éxito")
 
     def modificar_consulta(self):
         '''Solicita el id de una consulta.Busca la consulta  y actualiza su texto.modifica la fecha y hora de la consulta'''
         id = input("ingrese el id de la consulta a modificar: ")
         fecha = input("Ingrese la fecha correcta: ")
         hora = input("Ingrese la hora correcta: ")
-
-
         self.repositorioConsultas.update(id, fecha , hora)
 
     def modificar_cirugia(self):
-        print("TBD")
-
+        id = input("ingrese el id de la cirugia a modificar: ")
+        fecha = input("Ingrese la fecha correcta: ")
+        hora = input("Ingrese la hora correcta: ")
+        self.repositorioCirugias.update(id, fecha, hora)
 
     def eliminar_consulta(self):
         id = input("Ingrese el id de consulta a borrar")
-        self.repositorioConsultas.delete(id)
+        if self.repositorioConsultas.delete(id):
+            print("Consulta eliminada exitosamente")
 
     def eliminar_cirugia(self):
-        print("TBD")
+        id = input("Ingrese el id de la cirugia a borrar")
+        if self.repositorioCirugias.delete(id):
+            print("Cirugía eliminada exitosamente")
 
     def listar_consulta(self):
         consultas = self.repositorioConsultas.get_all()
+        print("Listado de consultas:")
         for i in consultas:
-             print(i.id, i.fecha, i.hora, i.numeroConsultorio, i.diagnostico, i.nombrePaciente)
-
+             i.mostrarPorPantalla()
 
     def listar_cirugia(self):
-        print("TBD")
+        print("Listado de cirugias:")
+        cirugia = self.repositorioCirugias.get_all()
+        for i in cirugia:
+            i.mostrarPorPantalla()
 
-
+    def nuevaReserva(self, tipoDeReserva):
+        hora = input("Ingrese la hora: ")
+        fecha = input("Ingrese la fecha: ")
+        nombrePaciente = input("Ingrese el nombre del paciente: ")
+        diagnostico = input("Ingrese el diagnostico: ")
+        if tipoDeReserva == "cirugia":
+            numeroQuirofano = input("Ingrese el numero del quirofano donde será operado: ")
+            horasAyuno = input("Ingrese las horas de ayuno necesarias antes de la cirugia: ")
+            instrumentadorQuirurgico = input("Ingrese el nombre del instrumentador quirurgico: ")
+            return Cirugia(fecha, hora, nombrePaciente, diagnostico, numeroQuirofano, horasAyuno, instrumentadorQuirurgico)
+        if tipoDeReserva == "consulta":
+            numeroConsultorio = input("Ingrese el numero de consultorio donde será atendido: ")
+            return Consulta(fecha, hora, nombrePaciente, diagnostico, numeroConsultorio)
 
 # Esta parte del código está fuera de la clase Menu.
 # Si este archivo es el programa principal, entonces llamamos al método
